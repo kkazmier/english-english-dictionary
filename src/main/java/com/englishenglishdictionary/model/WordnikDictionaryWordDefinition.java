f68cf422a9d60242c2f7e89f9e6e7e2dd0bc7b07
Karol <poczta.karol.kazmierczak@gmail.com>
2022-06-26 13:17:57 +0200

    First commit.

---
+++ src/main/java/com/englishenglishdictionary/model/WordnikDictionaryWordDefinition.java
@@ -0,0 +1,14 @@
+package com.englishenglishdictionary.model;
+
+public class WordnikDictionaryWordDefinition {
+    String text;
+    String partOfSpeech;
+
+    @Override
+    public String toString() {
+        return "WordnikDictionaryWordDefinition{" +
+                "text='" + text + '\'' +
+                ", partOfSpeech='" + partOfSpeech + '\'' +
+                '}';
+    }
+}
