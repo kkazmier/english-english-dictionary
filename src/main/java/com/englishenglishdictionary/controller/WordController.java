a5c26dc3432131105fb8902d4fe5cdab3785b9e7
Karol <poczta.karol.kazmierczak@gmail.com>
2022-06-26 13:17:01 +0200

    Add/comment method.

--- src/main/java/com/englishenglishdictionary/controller/WordController.java
+++ src/main/java/com/englishenglishdictionary/controller/WordController.java
@@ -2,6 +2,7 @@ package com.englishenglishdictionary.controller;
 
 
 import com.englishenglishdictionary.client.FreeDictionaryClient;
+import com.englishenglishdictionary.client.WordnikDictionaryClient;
 import com.englishenglishdictionary.model.Word;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,21 +13,28 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class WordController {
     private FreeDictionaryClient dictionaryClient;
+    private WordnikDictionaryClient wordnikDictionaryClient;
 
     @Autowired
-    public WordController(FreeDictionaryClient dictionaryClient) {
+    public WordController(FreeDictionaryClient dictionaryClient, WordnikDictionaryClient wordnikDictionaryClient) {
         this.dictionaryClient = dictionaryClient;
+        this.wordnikDictionaryClient = wordnikDictionaryClient;
     }
 
-    @GetMapping
-    Word getWordFromFreeDictionary(String word) {
-        Word result = new Word();
-
-        return result;
-    }
+//    @GetMapping
+//    Word getWordFromFreeDictionary(String word) {
+//        Word result = new Word();
+//
+//        return result;
+//    }
 
     @GetMapping("{word}")
     String getWordFromFreeDictionaryString(@PathVariable("word") String word) throws JsonProcessingException {
         return dictionaryClient.getWordFromFreeDictionary(word);
+    }
+
+    @GetMapping("wordnikDictionary/{word}")
+    String getWordDefinitions(@PathVariable("word") String word) throws JsonProcessingException {
+        return wordnikDictionaryClient.getDefinitions(word).toString();
     }
 }
