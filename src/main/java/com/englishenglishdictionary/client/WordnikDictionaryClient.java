3d00b401350190a94fe5428206115933c41a4963
Karol <poczta.karol.kazmierczak@gmail.com>
2022-07-09 18:04:05 +0200

    Clean.

--- src/main/java/com/englishenglishdictionary/client/WordnikDictionaryClient.java
+++ src/main/java/com/englishenglishdictionary/client/WordnikDictionaryClient.java
@@ -8,6 +8,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -36,10 +37,14 @@ public class WordnikDictionaryClient {
     public Collection<WordnikDictionaryWordDefinition> getDefinitions(String word) throws JsonProcessingException {
         String url = wordnikDictionaryEndpoint + "/" + word + "/" + "definitions" + "?api_key=" + wordnikApiKey;
         ResponseEntity<String> response = restTemplate.getForEntity(url, String.class);
+        logger.info("Request status code: " + response.getStatusCode());
         Type definitionsCollectionType = new TypeToken<ArrayList<WordnikDictionaryWordDefinition>>() { }.getType();
-        Collection<WordnikDictionaryWordDefinition> definitions = new Gson().fromJson(
-                response.getBody(), definitionsCollectionType);
-
-        return definitions;
+        if (response.getStatusCode() == HttpStatus.OK) {
+            Collection<WordnikDictionaryWordDefinition> definitions = new Gson().fromJson(
+                    response.getBody(), definitionsCollectionType);
+            return definitions;
+        } else {
+            return new ArrayList<>();
+        }
     }
 }
