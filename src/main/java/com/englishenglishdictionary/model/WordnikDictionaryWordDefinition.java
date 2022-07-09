b8e19a7e4314f805e6ad63994113b1219a55736e
Karol <poczta.karol.kazmierczak@gmail.com>
2022-07-09 18:05:10 +0200

    Add lombok annotations.

--- src/main/java/com/englishenglishdictionary/model/WordnikDictionaryWordDefinition.java
+++ src/main/java/com/englishenglishdictionary/model/WordnikDictionaryWordDefinition.java
@@ -1,5 +1,10 @@
 package com.englishenglishdictionary.model;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class WordnikDictionaryWordDefinition {
     String text;
     String partOfSpeech;
