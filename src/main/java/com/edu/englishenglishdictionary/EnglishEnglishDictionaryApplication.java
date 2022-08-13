5c7d7232ec563bef848a884ff894fa27e633bdb9
kkazmier <dev.karol.kazmierczak@gmail.com>
2022-08-13 20:48:43 +0200

    Initial commit.

---
+++ src/main/java/com/edu/englishenglishdictionary/EnglishEnglishDictionaryApplication.java
@@ -0,0 +1,13 @@
+package com.edu.englishenglishdictionary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnglishEnglishDictionaryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EnglishEnglishDictionaryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/edu/englishenglishdictionary/EnglishEnglishDictionaryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edu.englishenglishdictionary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnglishEnglishDictionaryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
