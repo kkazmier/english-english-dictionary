a54ea46f5cc014ca1c0a0466a000aa499a4f8834
kkazmier <dev.karol.kazmierczak@gmail.com>
2022-08-26 18:26:08 +0200

    Change controller.

--- src/main/java/com/edu/englishenglishdictionary/controller/WordnikDictionaryController.java
+++ src/main/java/com/edu/englishenglishdictionary/controller/WordnikDictionaryController.java
@@ -1,6 +1,7 @@
 package com.edu.englishenglishdictionary.controller;
 
 import com.edu.englishenglishdictionary.client.WordnikDictionaryClient;
+import com.edu.englishenglishdictionary.model.Word;
 import com.edu.englishenglishdictionary.model.WordnikDictionaryWordDefinition;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import org.slf4j.Logger;
@@ -8,9 +9,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Collection;
 import java.util.List;
@@ -27,12 +26,14 @@ public class WordnikDictionaryController {
         this.wordnikDictionaryClient = wordnikDictionaryClient;
     }
 
-    @GetMapping("/definitions/{word}")
-    String wordDefinitions(@PathVariable("word") String word, Model model) throws JsonProcessingException {
-        logger.info("Search defs for: " + word);
-        Collection<WordnikDictionaryWordDefinition> wordDefinitions = wordnikDictionaryClient.getDefinitions(word);
-        List<String> definitions = wordDefinitions.stream().map(
-                def -> def.getText()).collect(Collectors.toList());
+    @PostMapping("/definitions")
+    public String wordDefinitionsSubmit(@ModelAttribute("word") Word word, Model model) throws JsonProcessingException {
+        logger.info("submit " + word.getWord() + " definition");
+        Collection<WordnikDictionaryWordDefinition> wordDefinitions = wordnikDictionaryClient.getDefinitions(word.getWord());
+        List<String> definitions = wordDefinitions.stream()
+                .filter(e -> e.getText() != null)
+                .map(def -> def.getText())
+                .collect(Collectors.toList());
         model.addAttribute("definitions", definitions);
         return "wordnik-dictionary-definitions";
     }
